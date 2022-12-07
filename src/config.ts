966ec623d65fad9f0b19b68d05c7d34c7fb2330f
SNIVY <870667674@qq.com>
2022-12-07 22:03:19 +0800

    20221207

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const mysqlConfig = {
+	host: 'localhost',
+	port: 3306,
+	user: '',
+	password: '',
+	database: ''
+}
---
+++ src/model/index.ts
@@ -0,0 +1,13 @@
+import mysql from 'mysql';
+import { QueryOptions } from "mysql";
+import { mysqlConfig } from '@/config';
+
+const pool = mysql.createPool(mysqlConfig);
+export const query = (sql: QueryOptions | string, arr: any[]): Promise<any> => {
+	return new Promise((resolve, reject) => {
+		pool.query(sql, arr, (err, results, fields) => {
+			if (err) reject(err);
+			resolve(results);
+		})
+	})
+};
